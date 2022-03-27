bf8c24fff1150106d0dd7380b6b566e05655005c
FlySky-ovo <jeffyang0123@gmail.com>
2022-03-27 14:04:39 +0800

    bug .u.

--- cog/main.py
+++ cog/main.py
@@ -13,7 +13,7 @@ class Main(Cog_Extension):
 		emb = Embed(title = '幫助區 // 飛翔小助手')
 		emb.add_field(name='prefix `^`', value='前綴', inline=True)
 		emb.add_field(name='help', value='> 叫出這個沒啥用的幫助區', inline=True)
-		emb.add_field(name='ping', value='> 查看機器人延遲', inlne=True)
+		emb.add_field(name='ping', value='> 查看機器人延遲', inline=True)
 		emb.add_field(name='purge <count>', value='> 清除指定數量的訊息', inline=True)
 		await ctx.send(embed = emb)
 
