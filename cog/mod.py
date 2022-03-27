a8c7e5164d024d33351e9c5d1586f983363cecd7
FlySky-ovo <jeffyang0123@gmail.com>
2022-03-27 14:45:35 +0800

    cmd.purge

--- cog/mod.py
+++ cog/mod.py
@@ -2,11 +2,16 @@ from core.classes import Cog_Extension
 from discord.ext.commands import command
 
 class Mod(Cog_Extension):
+
 	@command()
-	async def purge(self, ctx, count):
-		ctx.purge(count)
-		await ctx.send(f'Done.')
-		print('mod.purge >> done')
+	async def say(self, ctx, *, msg):
+		await ctx.message.delete()
+		await ctx.send(msg)
+
+	@command()
+	async def purge(self, ctx, count: int):
+		await ctx.channel.purge(limit=count+1)
+		await ctx.send(f'Done')
 
 def setup(client):
 	client.add_cog(Mod(client))
